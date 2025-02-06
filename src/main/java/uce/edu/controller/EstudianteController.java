8148fd7861a8661865d1c49a935421b1e8fd8d60
DavidBoada08 <dfboadaa@uce.edu.ec>
2025-02-06 00:52:16 +0000

    Taller30

--- src/main/java/uce/edu/controller/EstudianteController.java
+++ src/main/java/uce/edu/controller/EstudianteController.java
@@ -7,6 +7,7 @@ import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import uce.edu.service.IEstudianteService;
 import uce.edu.to.EstudianteTo;
 
@@ -17,30 +18,30 @@ public class EstudianteController {
     private IEstudianteService iEstudianteService;
 
     @GET
-    @Path("/buscar")
-    public EstudianteTo buscarPorId() {
-        Integer id = 1;
+    @Path("/{id}")
+    public EstudianteTo buscarPorId(@PathParam("id") Integer id) {
+        //Integer id = 1;
 
         return this.iEstudianteService.buscarPorId(id);
         // return Response.ok(this.iEstudianteService.buscarPorId(id)).build();
     }
 
     @POST
-    @Path("/guardar")
+    @Path("")
     public void guardar(EstudianteTo estudiante) {
         this.iEstudianteService.guardar(estudiante);
 
     }
 
     @PUT
-    @Path("/actualizar")
+    @Path("")
     public void actualizar(EstudianteTo estudiante) {
         this.iEstudianteService.actualizar(estudiante);
 
     }
 
     @PATCH
-    @Path("/actualizar/parcial")
+    @Path("")
     public void actualizarParcial(EstudianteTo estudiante) {
         EstudianteTo tmp = this.iEstudianteService.buscarPorId(estudiante.getId());
         tmp.setNombre(estudiante.getNombre());
@@ -49,7 +50,7 @@ public class EstudianteController {
     }
 
     @DELETE
-    @Path("/borrar")
+    @Path("")
     public void borrar() {
         Integer id = 3;
         this.iEstudianteService.eliminar(id);
--- src/main/java/uce/edu/controller/PersonaController.java
+++ src/main/java/uce/edu/controller/PersonaController.java
@@ -7,7 +7,7 @@ import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
-
+import jakarta.ws.rs.PathParam;
 import uce.edu.service.IPersonaService;
 import uce.edu.to.PersonaTo;
 @Path("/personas")
@@ -17,31 +17,33 @@ public class PersonaController {
     private IPersonaService iPersonaService;
     
     @GET
-    @Path("/buscar")
-    public PersonaTo buscarPorId( ) {
-        Integer id = 3;
+    @Path("/{id}")
+    public PersonaTo buscarPorId( @PathParam("id") Integer id) {
+        //Integer id = 3;
         
         return this.iPersonaService.buscarPorId(id);
         //return Response.ok(this.iPersonaService.buscarPorId(id)).build();
     }
 
     @POST
-    @Path("/guardar")
+    @Path("")
     public void guardar(PersonaTo persona) {
         this.iPersonaService.guardar(persona);
 
     }
     @PUT
-    @Path("/actualizar")
-    public void actualizar(PersonaTo persona) {
+    @Path("/{id}")
+    public void actualizar(PersonaTo persona, @PathParam("id") Integer id) {    
+        persona.setId(id);
         this.iPersonaService.actualizar(persona);
 
     }
 
     @PATCH
-    @Path("/actualizar/parcial")
-    public void actualizarParcial(PersonaTo persona) {
-        PersonaTo tmp= this.iPersonaService.buscarPorId(persona.getId());
+    @Path("/{id}/nuevo/{cedula}")
+    public void actualizarParcial(PersonaTo persona, @PathParam("id") Integer id,String cedula) {
+        System.out.println(cedula);
+        PersonaTo tmp= this.iPersonaService.buscarPorId(id);
         tmp.setNombre(persona.getNombre());
         this.iPersonaService.actualizar(tmp);
 
@@ -50,9 +52,9 @@ public class PersonaController {
 
 
     @DELETE
-    @Path("/borrar")
-    public void borrar() {
-        Integer id = 3;
+    @Path("/{id}")
+    public void borrar(@PathParam("id") Integer id) {
+        //Integer id = 3;
     this.iPersonaService.eliminar(id);
 
     }
